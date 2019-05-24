2eb4eabb2a21353f84850d769430d56defaa7fd6
韦振琛 <346671169@qq.com>
2019-05-24 16:54:50 +0800

    1

--- js/inspinia.js
+++ js/inspinia.js
@@ -107,6 +107,13 @@ $(document).ready(function () {
         return false;
     });
 
+    // Append config box / Only for demo purpose
+    // Uncomment on server mode to enable XHR calls
+    //$.get("skin-config.html", function (data) {
+    //    if (!$('body').hasClass('no-skin-config'))
+    //        $('body').append(data);
+    //});
+
     // Minimalize menu
     $('.navbar-minimalize').on('click', function (event) {
         event.preventDefault();
